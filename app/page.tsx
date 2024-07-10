4668ee524863ad19708a2b44eb67bb40ac83896f
Mark <rosy@rosytown.com.au>
2024-07-11 09:44:09 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import type {FC} from 'react';
-import {Logo} from '@app/components/svg';
-import getFAQData from '@app/data/faq-data';
+import type { FC } from "react";
+import { Logo } from "@app/components/svg";
+import getFAQData from "@app/data/faq-data";
+import FAQSection from "@app/components/organisms/FAQSection";
 
 const Home: FC = (): React.ReactElement => {
 	// Use the data from getFAQData() to render the FAQ component
@@ -10,12 +11,19 @@ const Home: FC = (): React.ReactElement => {
 	return (
 		<main className="py-24">
 			<div className="container text-center">
-				<Logo className="w-64 mx-auto"/>
-				<h1 className="mt-8 text-lg max-w-[1024px] mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h1>
+				<Logo className="w-64 mx-auto" />
+				<h1 className="mt-8 text-lg max-w-[1024px] mx-auto">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat.
+				</h1>
 			</div>
 			<div className="mt-24 container">
-				{/* Replace the div below with the missing component */}
-				<div className="flex items-center justify-center border-2 border-dashed border-neutral-500 aspect-[16/7] rounded-lg"/>
+				<h2 className="text-center text-[22px] font-semibold mb-8">
+					Learn more about
+				</h2>
+				<FAQSection data={faqData.panels} />
 			</div>
 		</main>
 	);
